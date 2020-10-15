6bd587ba9cf2e2b7208e3c4a4b83ca34da7dddee
Garret Larson <garret.hs.larson@gmail.com>
2020-10-15 15:31:12 -0500

    GET req to GIPHY is working.

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -4,24 +4,25 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    console.log('in get request of /search endpoint', req.body);
-    let stringParameter = `?api_key=${process.env.GIPHY_API_KEY}`;//this is where your personal API key goes.
-    // if (req.params.tag) { 
-    //     stringParameter = stringParameter.concat(`&q=${req.params.tag}`);
-    // }
-    const { searchParameter } = req.body
-    stringParameter = req.body && stringParameter.concat(`&q=${searchParameter}`); //building the string up
-    console.log(stringParameter);
-  axios
-    .get(`api.giphy.com/v1/gifs/search${stringParameter}`)
-    .then((response) => {
-      console.log(response.data);
-      res.status(200).send(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).send(error);
-    });
-});
-
+    
+  let stringParameter = `?api_key=${process.env.GIPHY_API_KEY}`;//this is where your personal API key goes.
+  // if (req.params.tag) { 
+  //     stringParameter = stringParameter.concat(`&q=${req.params.tag}`);
+  // }
+  const { searchParameter } = req.body
+  stringParameter = stringParameter.concat(`&q=${searchParameter}`); //building the string up
+  
+  stringParameter = `https://api.giphy.com/v1/gifs/search${stringParameter}`
+  console.log(stringParameter);
+    axios
+      .get(`https://api.giphy.com/v1/gifs/search${stringParameter}`)
+      .then((response) => {
+        console.log(response.data.data[0].images.original.url);
+        res.status(200).send(response.data.data[0].images.original.url); //need to fix
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(400).send(error);
+      });
+  });
 module.exports = router;
